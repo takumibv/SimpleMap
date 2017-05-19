fc7589c0a478ff748c3eb6a7f1b6c7bbfce73432
takumibv <everybody.say.hey.yo@gmail.com>
2017-05-19 14:55:58 +0900

    現在地に移動するボタン追加

--- app/src/main/java/jp/ac/titech/itpro/sdl/simplemap/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/simplemap/MainActivity.java
@@ -10,6 +10,8 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -33,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements
     private GoogleApiClient googleApiClient;
     private LocationRequest locationRequest;
     private boolean requestingLocationUpdate;
+    private Location current_location = new Location("0");
 
     private enum UpdatingState {STOPPED, REQUESTING, STARTED}
 
@@ -67,6 +70,21 @@ public class MainActivity extends AppCompatActivity implements
         locationRequest.setInterval(10000);
         locationRequest.setFastestInterval(5000);
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        Button suggestButton = (Button)findViewById(R.id.move_current_location_btn);
+
+        suggestButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "btnClicked");
+                moveCurrentLocate();
+            }
+        });
+    }
+
+    public void moveCurrentLocate() {
+        googleMap.animateCamera(CameraUpdateFactory
+                .newLatLng(new LatLng(current_location.getLatitude(), current_location.getLongitude())));
     }
 
     @Override
@@ -121,8 +139,7 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public void onLocationChanged(Location location) {
         Log.d(TAG, "onLocationChanged: " + location);
-        googleMap.animateCamera(CameraUpdateFactory
-                .newLatLng(new LatLng(location.getLatitude(), location.getLongitude())));
+        current_location = location;
     }
 
     @Override
